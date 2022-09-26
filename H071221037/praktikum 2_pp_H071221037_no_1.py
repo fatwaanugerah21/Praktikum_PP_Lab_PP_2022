5e83c915e7f7d47311fa8d5de84f3a60d1aa2d32
MuhammadReza26 <rezazafirah66@gmail.com>
2022-09-26 09:18:47 +0800

    kumpul tugas 1

---
+++ H071221037/praktikum 2_pp_H071221037_no_1.py
@@ -0,0 +1,14 @@
+nilai = float(input('Masukkan nilai : '))
+
+if nilai >= 90:
+    print('A')
+elif nilai >= 80 and nilai < 90:
+    print('B')
+elif nilai >= 70 and nilai < 80:
+    print('C')
+elif nilai >= 60 and nilai < 70:
+    print('D')
+elif nilai >= 40 and nilai < 60:
+    print('E') 
+else:
+    print('F')                  
