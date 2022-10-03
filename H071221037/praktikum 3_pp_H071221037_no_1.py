f90f74f29e8729f11cf598d0733338d36270b55e
MuhammadReza26 <rezazafirah66@gmail.com>
2022-10-03 16:50:38 +0800

    kumpul tugas 3

---
+++ H071221037/praktikum 3_pp_H071221037_no_1.py
@@ -0,0 +1,13 @@
+x = int(input('masukkan nilai x : ')) #horizontal
+y = int(input('masukkan nilai y : ')) # vertikal
+angka = 1
+#syarat X < Y
+
+while angka <= y :
+    for i in range(x):
+        print(angka, end = " ")
+        angka = angka + 1
+    print()     
+
+    
+
